1688e3ed1c72089f03d0dcb3536460b7a2d480a0
Joloharo <joeljoanlopez@gmail.com>
2024-04-10 16:00:45 +0200

    Comienzo de implementacion de bullets con raycast

--- Assets/Scripts/Weapons/GunAttackHandler.cs
+++ Assets/Scripts/Weapons/GunAttackHandler.cs
@@ -3,6 +3,8 @@
 public class GunAttackHandler : MonoBehaviour
 {
     public float _aimDistance = 100f;
+    public Transform _gunPoint;
+    public GameObject _bulletTrail;
 
     private WeaponHolderController _controller;
     private LineRenderer _aimRay;
@@ -16,20 +18,25 @@ public class GunAttackHandler : MonoBehaviour
 
     public void Shoot()
     {
+        var _hit = Physics2D.Raycast(_gunPoint.position, _aimDirection, _aimDistance);
+        var _trail = Instantiate(_bulletTrail, _gunPoint.position, transform.rotation);
+        var _trailScript = _trail.GetComponent<BulletHandler>();
 
+        if (_hit.collider)
+        {
+            _trailScript.SetTargetPosition(_hit.point);
+        }
     }
 
     public void Update()
     {
+        // Calculate _aimDirection
         _aimDirection = Camera.main.ScreenToWorldPoint(Input.mousePosition) - transform.position;
         _aimDirection.z = 0;
 
+        // Set the _aimRay direction and show if needed
         _aimRay.SetPosition(0, transform.position);
         _aimRay.SetPosition(1, _aimDirection.normalized * _aimDistance);
-
-        if (_controller.Aiming)
-            _aimRay.enabled = true;
-        else
-            _aimRay.enabled = false;
+        _aimRay.enabled = _controller.enabled;
     }
 }
