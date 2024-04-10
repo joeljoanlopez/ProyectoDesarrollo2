bf50c3cc7fefc320f12975f53eba77aa133951b0
Joloharo <joeljoanlopez@gmail.com>
2024-04-10 17:07:24 +0200

    Revert "Merge branch 'main' of https://github.com/joeljoanlopez/ProyectoDesarrollo2"
    This reverts commit 1cb4007d3309f0544c19908dfe3b72ecc7c62a3e, reversing
    changes made to 251aeb37dcc5cd2325ebe20c44a456f90a12b2fc.

--- Assets/Scripts/Weapons/GunAttackHandler.cs
+++ Assets/Scripts/Weapons/GunAttackHandler.cs
@@ -3,8 +3,6 @@
 public class GunAttackHandler : MonoBehaviour
 {
     public float _aimDistance = 100f;
-    public Transform _gunPoint;
-    public GameObject _bulletTrail;
 
     private WeaponHolderController _controller;
     private LineRenderer _aimRay;
@@ -18,25 +16,20 @@ public class GunAttackHandler : MonoBehaviour
 
     public void Shoot()
     {
-        var _hit = Physics2D.Raycast(_gunPoint.position, _aimDirection, _aimDistance);
-        var _trail = Instantiate(_bulletTrail, _gunPoint.position, transform.rotation);
-        var _trailScript = _trail.GetComponent<BulletHandler>();
 
-        if (_hit.collider)
-        {
-            _trailScript.SetTargetPosition(_hit.point);
-        }
     }
 
     public void Update()
     {
-        // Calculate _aimDirection
         _aimDirection = Camera.main.ScreenToWorldPoint(Input.mousePosition) - transform.position;
         _aimDirection.z = 0;
 
-        // Set the _aimRay direction and show if needed
         _aimRay.SetPosition(0, transform.position);
         _aimRay.SetPosition(1, _aimDirection.normalized * _aimDistance);
-        _aimRay.enabled = _controller.enabled;
+
+        if (_controller.Aiming)
+            _aimRay.enabled = true;
+        else
+            _aimRay.enabled = false;
     }
 }
