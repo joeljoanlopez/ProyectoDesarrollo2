a23b26250b17fd2670c6debef4cca9e4c00fcd63
Yifan-AFK <yifan1647wang@gmail.com>
2024-04-19 15:51:49 +0200

    reset al morir

--- Assets/Scripts/Player/HealthManager.cs
+++ Assets/Scripts/Player/HealthManager.cs
@@ -1,13 +1,40 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class HealthManager : MonoBehaviour
 {
     public int _health = 100;
 
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.J))
+        {
+            RemoveHealth(20);
+        }
+        if (Input.GetKeyDown(KeyCode.K))
+        {
+            AddHealth(20);
+        }
+        if (_health <= 0)
+        {
+            RestartScene();
+        }
+    }
 
-    public void AddHealth(int value){
+    public void AddHealth(int value)
+    {
         _health += value;
+    }
+
+    public void RemoveHealth(int value)
+    {
+        _health -= value;
+    }
+
+    void RestartScene()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
     }
 }
