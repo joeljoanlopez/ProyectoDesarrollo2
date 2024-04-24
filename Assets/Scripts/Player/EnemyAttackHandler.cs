b645b116694a60ad3a1e7ca424c2d87235e99382
VoltorH <darkkrasher@gmail.com>
2024-04-24 17:22:33 +0200

    Ataque arreglado

--- Assets/Scripts/Player/EnemyAttackHandler.cs
+++ Assets/Scripts/Player/EnemyAttackHandler.cs
@@ -7,28 +7,36 @@ public class EnemyAttackHandler : MonoBehaviour
     // Start is called before the first frame update
     private SpriteRenderer _claw;
     private Collider2D _clawCollider;
+    private float _timer;
+
     public float _damage = 30;
+    public float _cooldown = 2f; 
     void Start()
     {
      _claw = GetComponent<SpriteRenderer>();
      _clawCollider = GetComponent<Collider2D>();
      _claw.enabled = false;
      _clawCollider.enabled = false;
+        _timer = _cooldown;
         
     }
 
     // Update is called once per frame
     void Update()
     {
-
+        _timer = _timer - Time.deltaTime;
     }
 public IEnumerator Strike()
 {
-    _claw.enabled = true;
-    _clawCollider.enabled = true;
-    yield return new WaitForSeconds(1f);
-    _claw.enabled = false;
-    _clawCollider.enabled = false;
+        if (_timer <= 0)
+        {
+            _claw.enabled = true;
+            _clawCollider.enabled = true;
+            yield return new WaitForSeconds(0.1f);
+            _claw.enabled = false;
+            _clawCollider.enabled = false;
+            _timer = _cooldown;
+        }
 }
 private void OnTriggerEnter2D(Collider2D other)
 {
