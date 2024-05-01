a79125b17aa3af4f3f381b0978831d9a6357a855
Joloharo <joeljoanlopez@gmail.com>
2024-05-01 12:06:38 +0200

    Update LightManager.cs

--- Assets/Scripts/Managing/LightManager.cs
+++ Assets/Scripts/Managing/LightManager.cs
@@ -1,5 +1,19 @@
 using UnityEngine;
 
-public class LightManager : MonoBehaviour {
-    
+public class LightManager : MonoBehaviour
+{
+    public GameObject _levelParent;
+    [SerializeField]private GameObject[] _levels;
+    [SerializeField]private Light[][] _lights;
+    private int _levelNumber;
+
+    private void Start() {
+        _levelNumber = _levelParent.transform.childCount;
+        _levels = new GameObject[_levelNumber];
+        _lights = new Light [_levelNumber][];
+        for (int i = 0; i < _levelNumber; i++){
+            _levels[i] = _levelParent.transform.GetChild(i).gameObject;
+            _lights[i] = GetComponentsInChildren<Light>();
+        }
+    }
 }
