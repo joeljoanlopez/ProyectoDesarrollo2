d784ef1072df7cd86ac17066d902a85a39358c47
Joloharo <joeljoanlopez@gmail.com>
2024-05-01 12:42:17 +0200

    Luces dinámicas implementadas

--- Assets/Scripts/Managing/LightManager.cs
+++ Assets/Scripts/Managing/LightManager.cs
@@ -1,19 +1,55 @@
 using UnityEngine;
+using UnityEngine.Rendering.Universal;
 
 public class LightManager : MonoBehaviour
 {
+    public Collider2D _player;
     public GameObject _levelParent;
-    [SerializeField]private GameObject[] _levels;
-    [SerializeField]private Light[][] _lights;
+    [SerializeField] private GameObject[] _levels;
+    [SerializeField] private Light2D[][] _lights;
     private int _levelNumber;
 
-    private void Start() {
+    private void Start()
+    {
         _levelNumber = _levelParent.transform.childCount;
         _levels = new GameObject[_levelNumber];
-        _lights = new Light [_levelNumber][];
-        for (int i = 0; i < _levelNumber; i++){
+        _lights = new Light2D[_levelNumber][];
+
+        for (int i = 0; i < _levelNumber; i++)
+        {
             _levels[i] = _levelParent.transform.GetChild(i).gameObject;
-            _lights[i] = GetComponentsInChildren<Light>();
+            _lights[i] = _levels[i].GetComponentsInChildren<Light2D>();
+
+            for (int j = 0; j < _lights[i].Length; j++)
+            {
+                _lights[i][j].enabled = false;
+            }
+        }
+    }
+
+    private void Update()
+    {
+        bool _found = false;
+        int _currentLevel = 0;
+
+        while (!_found && _currentLevel < _levels.Length)
+        {
+            Collider2D[] _colliders = _levels[_currentLevel].GetComponentsInChildren<Collider2D>();
+            for (int i = 0; i < _colliders.Length; i++)
+            {
+                if (_colliders[i].IsTouching(_player))
+                    _found = true;
+            }
+
+            if (!_found) _currentLevel++;
+        }
+
+        for (int i = 0; i < _lights.Length; i++)
+        {
+            for (int j = 0; j < _lights[i].Length; j++)
+            {
+                _lights[i][j].enabled = (i == _currentLevel);
+            }
         }
     }
 }
