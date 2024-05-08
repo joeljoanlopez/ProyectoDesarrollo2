591e709d19b4728b4a37ef2035d459da1a8af62e
VoltorH <darkkrasher@gmail.com>
2024-05-08 19:55:00 +0200

    Main Menu

---
+++ Assets/Scripts/Menus/MainMenu.cs
@@ -0,0 +1,16 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public void playGame()
+    {
+        SceneManager.LoadScene(0);
+    }
+
+    public void quitGame()
+    {
+        Application.Quit();
+    }
+}
