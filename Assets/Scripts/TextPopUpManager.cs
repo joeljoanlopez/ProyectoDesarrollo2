7bedd2d03a153f97b176672ea046cfac7a191751
Joloharo <joeljoanlopez@gmail.com>
2024-05-23 22:02:53 +0200

    Texto Arreglado

--- Assets/Scripts/TextPopUpManager.cs
+++ Assets/Scripts/TextPopUpManager.cs
@@ -11,11 +11,13 @@ public class TextPopUpManager : MonoBehaviour
     public float _textTime = 0;
     private TextMeshProUGUI _textComponent;
     private int _alea;
+    private bool _canTalk;
 
     AudioManager _audioManager;
     private void Awake()
     {
         _audioManager = GameObject.FindGameObjectWithTag("Audio").GetComponent<AudioManager>();
+        _canTalk = true;
     }
     void Update()
     {
@@ -29,37 +31,35 @@ public class TextPopUpManager : MonoBehaviour
                 _textTime = 0;
             }
         }
+
+        _canTalk = !_activeText;
     }
 
     public void ShowText(string message)
     {
-        _textPopUp.SetActive(true);
         if (_textComponent == null)
-        {
             _textComponent = _textPopUp.GetComponentInChildren<TextMeshProUGUI>();
-        }
-        else if (!_textPopUp.activeSelf)
+        _textPopUp.SetActive(true);
+        if (_canTalk)
         {
+            StartCoroutine(AnimateText(message));
         }
-
-        _textComponent.text = "";
-        StopCoroutine("AnimateText");    
-
-        StartCoroutine(AnimateText(message));
     }
 
     private IEnumerator AnimateText(string message)
     {
         int i = 0;
+        _textComponent.text = "";
+        _textTime = 0;
         _activeText = true;
         while (i < message.Length)
         {
-            _alea = Random.Range(0, 5);
-
             _textComponent.text += message[i];
 
+            _alea = Random.Range(0, 5);
+
             if (_alea == 0) _audioManager.PlaySFX(_audioManager.Huh1);
-            
+
             if (_alea == 1) _audioManager.PlaySFX(_audioManager.Huh2);
 
             if (_alea == 2) _audioManager.PlaySFX(_audioManager.Huh3);
@@ -70,7 +70,7 @@ public class TextPopUpManager : MonoBehaviour
 
             if (_alea == 5) _audioManager.PlaySFX(_audioManager.Huh6);
 
-            yield return new WaitForSeconds(0.05f); 
+            yield return new WaitForSeconds(0.05f);
 
             i++;
         }
