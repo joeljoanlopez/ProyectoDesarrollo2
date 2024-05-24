a10d6e00d3e33c5918819aa4c5a4199bd887aa7a
Joloharo <joeljoanlopez@gmail.com>
2024-05-24 11:43:57 +0200

    Animacion de muerte hecha

--- Assets/Scripts/Enemy/FSM/DieBehavior.cs
+++ Assets/Scripts/Enemy/FSM/DieBehavior.cs
@@ -1,14 +1,22 @@
+using System.Collections;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class DieBehavior : StateMachineBehaviour
 {
+    private float _timer;
+
     public override void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
     {
-
+        animator.SetTrigger("Dead");
     }
+
+    public override void OnStateUpdate(Animator animator, AnimatorStateInfo animatorStateInfo, int layerIndex)
+    {
+    }
+
     public override void OnStateExit(Animator animator, AnimatorStateInfo animatorStateInfo, int layerIndex)
     {
-
         Destroy(animator.gameObject);
     }
 }
