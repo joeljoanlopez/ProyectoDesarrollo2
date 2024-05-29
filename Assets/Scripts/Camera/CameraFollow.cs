1006cc0f77562609d6b27889e6ea66e1ee57962c
Joloharo <joeljoanlopez@gmail.com>
2024-05-29 18:16:07 +0200

    Animacion de disparo medio arreglada

--- Assets/Scripts/Camera/CameraFollow.cs
+++ Assets/Scripts/Camera/CameraFollow.cs
@@ -7,8 +7,13 @@ public class CameraFollow : MonoBehaviour
 {
     public bool _smooth = true;
     public Transform _target;
+    public float _smoothTime = 0.1f;
     private Vector3 _offset = new Vector3(0, 2, -10);
-    private float _smoothTime = 0.1f;
+    private MovementController _targetController;
+
+    private void Start() {
+        _targetController = _target.GetComponent<MovementController>();
+    }
 
     // Update is called once per frame
     void Update()
@@ -16,9 +21,9 @@ public class CameraFollow : MonoBehaviour
         if (_target != null)
         {
             // Calculate the offset based on the player's horizontal movement
-            if (_target.GetComponent<MovementController>()._horizontal != 0)
+            if (_targetController._horizontal != 0)
             {
-                _offset.x = _target.GetComponent<MovementController>()._horizontal * 3;
+                _offset.x = _targetController._horizontal * 3;
             }
 
             if (_smooth)
