0a1243f536fe06219fe198312add031ac619bb4d
VoltorH <130765229+VoltorH@users.noreply.github.com>
2024-06-27 19:42:06 +0200

    50% del sistema de inventario hecho!

--- Assets/Scripts/Inventory/Grid/GridClass.cs
+++ Assets/Scripts/Inventory/Grid/GridClass.cs
@@ -2,13 +2,14 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class GridClass
+public class GridClass<T>
 {
     private int width;
     private int height;
     private float cellSize;
-    private int[,] gridArray;
+    private T[,] gridArray;
     private TextMesh[,] textMeshArray;
+    private GameObject[,] gameObjectArray;
     private Vector3 originPosition;
 
     public GridClass(int width, int height, float cellSize, Vector3 originPosition = default(Vector3))
@@ -18,17 +19,17 @@ public class GridClass
         this.cellSize = cellSize;
         this.originPosition = originPosition;
 
-        gridArray = new int[width, height];
+        gridArray = new T[width, height];
         textMeshArray = new TextMesh[width, height];
+        gameObjectArray = new GameObject[width, height];
 
         for (int x = 0; x < gridArray.GetLength(0); x++)
         {
             for (int y = 0; y < gridArray.GetLength(1); y++)
             {
-                textMeshArray[x, y] = WorldText.CreateWorldText(gridArray[x, y].ToString(), null, GetWorldPoint(x, y) + new Vector3(cellSize,cellSize)* 0.5f,5, Color.white, TextAnchor.MiddleCenter);
-                Debug.DrawLine(GetWorldPoint(x,y), GetWorldPoint(x,y+1), Color.white, 100f);
-                                Debug.DrawLine(GetWorldPoint(x,y), GetWorldPoint(x+1, y ), Color.white, 100f);
-
+                textMeshArray[x, y] = WorldText.CreateWorldText(gridArray[x, y]?.ToString(), null, GetWorldPoint(x, y) + new Vector3(cellSize, cellSize) * 0.5f, 5, Color.white, TextAnchor.MiddleCenter);
+                Debug.DrawLine(GetWorldPoint(x, y), GetWorldPoint(x, y + 1), Color.white, 100f);
+                Debug.DrawLine(GetWorldPoint(x, y), GetWorldPoint(x + 1, y), Color.white, 100f);
             }
         }
     }
@@ -44,12 +45,12 @@ public class GridClass
         y = Mathf.FloorToInt((worldPosition - originPosition).y / cellSize);
     }
 
-    public void SetValue(int x, int y, int value)
+    public void SetValue(int x, int y, T value)
     {
         if (x >= 0 && y >= 0 && x < width && y < height)
         {
             gridArray[x, y] = value;
-            textMeshArray[x, y].text = value.ToString();  // Update the displayed text
+            textMeshArray[x, y].text = value.ToString();
         }
         else
         {
@@ -57,13 +58,97 @@ public class GridClass
         }
     }
 
-    public void SetValue(Vector3 worldPosition, int value)
+    public void SetValue(Vector3 worldPosition, T value)
     {
         int x, y;
         GetXY(worldPosition, out x, out y);
         SetValue(x, y, value);
     }
+
+    public void AddGameObject(Vector3 worldPosition, GameObject gameObjectPrefab)
+    {
+        int x, y;
+        GetXY(worldPosition, out x, out y);
+        if (x >= 0 && y >= 0 && x < width && y < height)
+        {
+            if (gameObjectArray[x, y] != null)
+            {
+                // Destroy the existing GameObject before adding a new one
+                GameObject.Destroy(gameObjectArray[x, y]);
+            }
+            Vector3 position = GetWorldPoint(x, y) + new Vector3(cellSize, cellSize) * 0.5f;
+            gameObjectArray[x, y] = GameObject.Instantiate(gameObjectPrefab, position, Quaternion.identity);
+        }
+        else
+        {
+            Debug.LogWarning($"AddGameObject: Index out of range. x: {x}, y: {y}");
+        }
+    }
+
+    public GameObject GetGameObject(Vector3 worldPosition)
+    {
+        int x, y;
+        GetXY(worldPosition, out x, out y);
+        if (x >= 0 && y >= 0 && x < width && y < height)
+        {
+            return gameObjectArray[x, y];
+        }
+        return null;
+    }
+
+    public bool IsEmpty(int x, int y)
+    {
+        if (x >= 0 && y >= 0 && x < width && y < height)
+        {
+            return gameObjectArray[x, y] == null;
+        }
+        return false;
+    }
+
+    public void RemoveGameObject(Vector3 worldPosition)
+    {
+        int x, y;
+        GetXY(worldPosition, out x, out y);
+        if (x >= 0 && y >= 0 && x < width && y < height)
+        {
+            if (gameObjectArray[x, y] != null)
+            {
+                GameObject.Destroy(gameObjectArray[x, y]);
+                gameObjectArray[x, y] = null;
+            }
+        }
+    }
+
+public Vector3? FindNearestEmptyCell(Vector3 worldPosition)
+{
+    int startX, startY;
+    GetXY(worldPosition, out startX, out startY);
+
+    float minDistance = float.MaxValue;
+    Vector3? nearestEmptyCell = null;
+
+    for (int x = 0; x < width; x++)
+    {
+        for (int y = 0; y < height; y++)
+        {
+            if (IsEmpty(x, y))
+            {
+                Vector3 cellPosition = GetWorldPoint(x, y) + new Vector3(cellSize, cellSize) * 0.5f;
+                float distance = Vector3.Distance(worldPosition, cellPosition);
+
+                if (distance < minDistance)
+                {
+                    minDistance = distance;
+                    nearestEmptyCell = cellPosition;
+                }
+            }
+        }
+    }
+
+    return nearestEmptyCell;
 }
+}
+
 
 public static class WorldText
 {
@@ -91,6 +176,3 @@ public static class WorldText
         return textMesh;
     }
 }
-
-
-
--- Assets/Scripts/Inventory/Grid/Testing.cs
+++ Assets/Scripts/Inventory/Grid/Testing.cs
@@ -4,21 +4,56 @@ using UnityEngine;
 
 public class Testing : MonoBehaviour
 {
-    private GridClass grid;
+    private GridClass<int> grid;
+    public GameObject gameObjectPrefab;
+
+    private GameObject selectedGameObject;
+    private Vector3 initialPosition;
 
     private void Start()
     {
-        Vector3 originPosition = Camera.main.ScreenToWorldPoint(new Vector3(0, 0, 1));
-        grid = new GridClass(20, 10, 2f, originPosition);
+        Vector3 originPosition = Camera.main.ScreenToWorldPoint(new Vector3(0, 0, Camera.main.nearClipPlane));
+        originPosition.z = 0;  // Ensure the z-coordinate is 0
+        grid = new GridClass<int>(20, 10, 1f, originPosition);
     }
 
     private void Update()
     {
+        Vector3 mouseWorldPosition = GetMouseWorldPosition();
+        Debug.Log($"Mouse World Position: {mouseWorldPosition}");
+
         if (Input.GetMouseButtonDown(0))
         {
-            Vector3 mouseWorldPosition = GetMouseWorldPosition();
-            Debug.Log($"Mouse World Position: {mouseWorldPosition}");
-            grid.SetValue(mouseWorldPosition, 56);
+            selectedGameObject = grid.GetGameObject(mouseWorldPosition);
+            if (selectedGameObject != null)
+            {
+                initialPosition = mouseWorldPosition;
+            }
+            else
+            {
+                grid.SetValue(mouseWorldPosition, 56);  // Set the value to an int
+                grid.AddGameObject(mouseWorldPosition, gameObjectPrefab);  // Add the GameObject
+            }
+        }
+
+        if (selectedGameObject != null)
+        {
+            selectedGameObject.transform.position = mouseWorldPosition;
+
+            if (Input.GetMouseButtonUp(0))
+            {
+                grid.RemoveGameObject(initialPosition);
+                Vector3? nearestEmptyCell = grid.FindNearestEmptyCell(mouseWorldPosition);
+                if (nearestEmptyCell.HasValue)
+                {
+                    grid.AddGameObject(nearestEmptyCell.Value, selectedGameObject);
+                }
+                else
+                {
+                    grid.AddGameObject(initialPosition, selectedGameObject);
+                }
+                selectedGameObject = null;
+            }
         }
     }
 
@@ -27,6 +62,16 @@ public class Testing : MonoBehaviour
         Vector3 vec = GetMouseWorldPositionWithZ(Input.mousePosition, Camera.main);
         vec.z = 0f;
         return vec;
+    }
+
+    public static Vector3 GetMouseWorldPositionWithZ()
+    {
+        return GetMouseWorldPositionWithZ(Input.mousePosition, Camera.main);
+    }
+
+    public static Vector3 GetMouseWorldPositionWithZ(Camera worldCamera)
+    {
+        return GetMouseWorldPositionWithZ(Input.mousePosition, worldCamera);
     }
 
     public static Vector3 GetMouseWorldPositionWithZ(Vector3 screenPosition, Camera worldCamera)
