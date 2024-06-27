257a32c5ae60b10ae222bc7ae2d30a28fc85e43c
VoltorH <130765229+VoltorH@users.noreply.github.com>
2024-06-27 17:06:29 +0200

    Grid acabada (dios santo bendito apiadate de mi)

---
+++ Assets/Scripts/Inventory/Grid/Testing.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Testing : MonoBehaviour
+{
+    private GridClass grid;
+
+    private void Start()
+    {
+        Vector3 originPosition = Camera.main.ScreenToWorldPoint(new Vector3(0, 0, 1));
+        grid = new GridClass(20, 10, 2f, originPosition);
+    }
+
+    private void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            Vector3 mouseWorldPosition = GetMouseWorldPosition();
+            Debug.Log($"Mouse World Position: {mouseWorldPosition}");
+            grid.SetValue(mouseWorldPosition, 56);
+        }
+    }
+
+    public static Vector3 GetMouseWorldPosition()
+    {
+        Vector3 vec = GetMouseWorldPositionWithZ(Input.mousePosition, Camera.main);
+        vec.z = 0f;
+        return vec;
+    }
+
+    public static Vector3 GetMouseWorldPositionWithZ(Vector3 screenPosition, Camera worldCamera)
+    {
+        Vector3 worldPosition = worldCamera.ScreenToWorldPoint(screenPosition);
+        return worldPosition;
+    }
+}
